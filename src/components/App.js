bf4c9fff01ee94182eaaa9c1edb5d2cbf509ad32
kyungyoonha <gkb10a@naver.com>
2020-07-08 19:43:21 +0900

    Initialize React Project

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (<div>
+            <h3>Hello World</h3>
+        </div>);
+    }
+}
+
+export default App;
